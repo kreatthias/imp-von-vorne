ec60e6aa18d4673fe411cf3f34a321501db14520
kreatthias <matthias.miro@gmail.com>
2016-12-16 16:05:16 +0100

    a little bit of uframe

---
+++ Assets/Code/Systems/DamageSystem.cs
@@ -0,0 +1,16 @@
+namespace Matze {
+    using System;
+    using System.Collections;
+    using System.Collections.Generic;
+    using System.Linq;
+    using uFrame.ECS.APIs;
+    using uFrame.ECS.Components;
+    using uFrame.ECS.Systems;
+    using uFrame.Kernel;
+    using UniRx;
+    using UnityEngine;
+    
+    
+    public partial class DamageSystem {
+    }
+}
